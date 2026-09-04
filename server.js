4cdeacf4a88f88e0b6a0beccb24b585bebc0146c
Falak gor <gorfalak2@gmail.com>
2026-09-04 17:51:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ async function startServer() {
             }
         });
 
-        const PORT = 3000;
+        const PORT =  process.env.PORT || 3000;
 
         app.listen(PORT, () => {
             console.log(`Server running on http://localhost:${PORT}`);
